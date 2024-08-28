61702dc6c85c7423bf81107e9aad1d803009d80b
Edgar Lopez <edgarlopez755@gmail.com>
2024-08-27 20:12:14 -0700

    Finish Exercise 2

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const app = express()
 
-// Excercise 1 Be Polite, Greet the User
+// Exercise 1 Be Polite, Greet the User
+
 app.get('/greetings/:name', (req, res) => {
     const name = req.params.name
     res.send(`Hello there, ${name}!`)
+})
+
+// Exercise 2 Rolling the Dice
+
+app.get('/roll/:number', (req, res) => {
+    const number = (req.params.number)
+    if (isNaN(number)) {
+        res.send('You must specify a number.')
+    } else {
+        const randomNumber = Math.floor(Math.random() * (number + 1))
+        res.send(`You rolled a ${randomNumber}.`)
+    }
 })
 
 
 
 
 
-
-
 app.listen(3000, () => {
-    console.log('Server listening on port 3000')
+    console.log('Listening on port 3000 ')
 })
